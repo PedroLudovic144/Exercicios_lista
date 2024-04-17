227dc45fdeae548d798fb9fad540a162ba02e534
PedroLudovic144 <136478720+PedroLudovic144@users.noreply.github.com>
2024-04-17 09:49:38 -0300

    Add files via upload

---
+++ exerc_funcao/ex1.py
@@ -0,0 +1,9 @@
+lista = list()
+def preenche_lista(lista):
+    for i in range(0,5,1):
+      x = input(print(f"Digite valor de {i}: "))
+      lista.append(x)
+preenche_lista(lista)
+print(lista)
+        
+    
