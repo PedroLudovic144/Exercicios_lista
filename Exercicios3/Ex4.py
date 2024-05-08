7fca6a262380249f06715ca026e54424b66e2035
PedroLudovic144 <pedroludovic5@gmail.com>
2024-05-08 08:37:13 -0300

    Commit

--- Exercicios3/Ex4.py
+++ Exercicios3/Ex4.py
@@ -1,10 +1,17 @@
-def retorna_indice(elemento):
-    n = int(input("Digite o índice do valor a ser procurado: "))  # Convertendo para inteiro
+def retorna_indice(elemento,n):
     for i in range(len(elemento)):
-        if i == n:
+        if elemento == n:
             return elemento[i]
-    return -1
+        else:
+            return -1
+        
+import os
+os.system("cls")
 
+n = int(input("Digite o índice do valor a ser procurado: "))
 lista1 = [5, 7, 8, 9, 7]
-resultado = retorna_indice(lista1)
+resultado = retorna_indice(lista1,5)
 print(resultado)
+#Pegue uma lista e retorne todos os indeces de uma lista
+#Retornar true se uma LISTA SÓ CONTEM DADOS INTEIROS
+#29/05/
